c1d2ab0a07966aff1f4dd4a41812a2a6d530b826
Reddi1931 <reddi1931@gmail.com>
2024-07-22 15:09:39 +0530

    Conflicts Resolved Successfully

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -14,7 +14,7 @@ const HeroSection = () => {
     return (
         <div className="hero-section flex flex-col justify-center items-center mt-[80px] h-fit w-screen py-[30px] pb-[40px] box-border text-white bg-black">
             <div className="hero-section-stats-wrapper flex justify-between mb-[55px] h-fit w-[80%] sm:flex-wrap sm:justify-evenly sm:w-full sm:mb-[30px]">
-                <div className="hero-section-stat border-r border-[#797979] sm:border-none flex-grow sm:border-none sm:flex-grow-0 s-one">
+                <div className="hero-section-stat border-r border-[#797979] flex-grow sm:border-none sm:flex-grow-0 s-one">
                     <div className="stat-header text-[30px] font-bold text-center text-[var(--theme-yellow)] sm:text-[18px]">24 Years</div>
                     <div className="stat-sub-header text-[18px] font-bold text-center sm:text-[14px]">Of Service Excellence</div>
                 </div>
@@ -24,7 +24,7 @@ const HeroSection = () => {
                     <div className="stat-sub-header text-[18px] font-bold text-center sm:text-[14px]">Happy Customers</div>
                 </div>
 
-                <div className="hero-section-stat border-r border-[#797979] sm:border-none flex-grow sm:border-none sm:flex-grow-0 s-three">
+                <div className="hero-section-stat border-r border-[#797979]  flex-grow sm:border-none sm:flex-grow-0 s-three">
                     <div className="stat-header text-[30px] font-bold text-center text-[var(--theme-yellow)] sm:text-[18px]">600+</div>
                     <div className="stat-sub-header text-[18px] font-bold text-center sm:text-[14px]">Corporates Served</div>
                 </div>
@@ -34,7 +34,6 @@ const HeroSection = () => {
                     <div className="stat-sub-header text-[18px] font-bold text-center sm:text-[14px]">Servicing Cities</div>
                 </div>
             </div>
-            
             <FindYourRide />
             <TravelExperience />
             <TravelSolutions />
